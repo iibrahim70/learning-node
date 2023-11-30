8c8f9a67085a9cfc6b68b9188600591f0cf11078
iibrahim70 <aviknuhash70@gmail.com>
2023-11-30 13:54:54 +0600

    added async file system

---
+++ async-file.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+// reading text asynchronoulsy
+fs.readFile("./text/read.txt", 'utf-8', (err, data) => {
+  if (err) {
+    throw Error("Error Reading Text");
+  }
+  console.log(data);
+
+  // writing text asynchronoulsy
+  fs.writeFile("./text/write.txt", data, "utf-8", (err) => {
+    if (err) {
+      throw Error("Error Writing Text");
+    }
+  });
+});
+
+  console.log('Testing asynchronous');
