44e8e5ee60f864b97c7e8c21334ec4cddf7b30d0
iibrahim70 <aviknuhash70@gmail.com>
2023-11-30 13:41:40 +0600

    added module system

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // local module
 
-// const add = require("./local1");
 const { add1, add2 } = require("./local1");
 
 console.log(add1(2, 3));
 console.log(add2(2, 7, 5));
+
+// built in module
+const path = require("path");
+console.log(path);
--- local1.js
+++ local1.js
@@ -1,7 +1,4 @@
 const add1 = (param1, param2) => param1 + param2;
 const add2 = (param1, param2, param3) => param1 + param2 + param3;
 
-const vara = () => {
-  fsdkf;
-};
 module.exports = { add1, add2 };
